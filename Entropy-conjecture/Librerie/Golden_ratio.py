29b5b4fa6c993a26c43258f8a1cb4baab35e0720
giacomobrugali <giacomo.brugali@gmail.com>
2026-04-16 14:10:32 +0200

    updating libraries

--- Entropy-conjecture/Librerie/Golden_ratio.py
+++ Entropy-conjecture/Librerie/Golden_ratio.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import likelihood as lh
+import matplotlib.pyplot as plt
 
 def GR_iter_min ( g, x_min, x_max, prec) :
    
@@ -26,7 +27,7 @@ def GR_iter_min ( g, x_min, x_max, prec) :
          
    
    
-def GR_iter_max ( g, x_min, x_max, prec) :
+'''def GR_iter_max ( g, x_min, x_max, prec) :
    
    x_1 = 0.
    x_2 = 0.
@@ -45,7 +46,7 @@ def GR_iter_max ( g, x_min, x_max, prec) :
          x_max = x_1 
                
       L = abs(x_min - x_max)
-   return (x_min + x_max) / 2         
+   return (x_min + x_max) / 2         '''
    
    
 def GR_rec_min ( g, x_min, x_max, prec) :
@@ -87,7 +88,36 @@ def GR_rec_max_LL (pdf , sample, x_min, x_max, prec) :
    tau_hat = GR_iter_max ( g, x_min, x_max, prec)  
    return tau_hat 
    
-   
+def GR_iter_max(g, x_min, x_max, prec):
+    r = 0.618
+    inv_r = 1.0 - r
+    
+    # Inizializziamo i due punti interni
+    # x1 è a destra, x2 è a sinistra
+    x2 = x_min + inv_r * (x_max - x_min)
+    x1 = x_min + r * (x_max - x_min)
+    
+    # Calcoliamo i valori iniziali (solo una volta!)
+    f1 = g(x1)
+    f2 = g(x2)
+    
+    while abs(x_max - x_min) > prec:
+        if f2 < f1:
+            # Il massimo è a destra, restringo da sinistra
+            x_min = x2
+            x2 = x1      # Riciclo il punto vecchio
+            f2 = f1      # Riciclo il valore vecchio
+            x1 = x_min + r * (x_max - x_min)
+            f1 = g(x1)   # UNICA nuova chiamata alla funzione
+        else:
+            # Il massimo è a sinistra, restringo da destra
+            x_max = x1
+            x1 = x2      # Riciclo il punto vecchio
+            f1 = f2      # Riciclo il valore vecchio
+            x2 = x_min + inv_r * (x_max - x_min)
+            f2 = g(x2)   # UNICA nuova chiamata alla funzione
+            
+    return (x_min + x_max) / 2   
    
    
    
